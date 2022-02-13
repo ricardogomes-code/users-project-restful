e0749090e4445eca97cebb167dfd9dff191e0cf1
Ricardo Gomes <ricardogoncalvesgomes@gmail.com>
2022-02-13 19:54:07 +0000

    U02 - restify in client-server

--- client-server/routes/users.js
+++ client-server/routes/users.js
@@ -1,9 +1,23 @@
 var express = require('express');
+var assert = require('assert');
+var restify = require('restify-clients');
 var router = express.Router();
 
+// Creates a JSON client
+var client = restify.createJsonClient({
+  url: 'http://localhost:4000'
+});
+//http and no https
+
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', function (req, res, next) {
+
+  client.get('/users', function (err, request, response, obj) {
+
+    assert.ifError(err);
+    res.end(JSON.stringify(obj, null, 2));
+  });
+
 });
 
 module.exports = router;
